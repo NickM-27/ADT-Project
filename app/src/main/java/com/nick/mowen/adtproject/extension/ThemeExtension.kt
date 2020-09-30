39634e6049d193f9356cdae94f6d56cea02a2529
Nick Mowen <nickmowen213@gmail.com>
2020-09-30 13:43:38 -0500

    Make layout edge to edge and hide shadow overlay

--- app/src/main/java/com/nick/mowen/adtproject/extension/ThemeExtension.kt
+++ app/src/main/java/com/nick/mowen/adtproject/extension/ThemeExtension.kt
@@ -1,15 +1,25 @@
 package com.nick.mowen.adtproject.extension
 
+import android.app.Activity
 import android.content.Context
+import android.os.Build
 import androidx.databinding.ViewDataBinding
 import com.nick.mowen.adtproject.databinding.ActivityMainBinding
 import com.nick.mowen.adtproject.databinding.FragmentCharacterListBinding
+
+private fun Context.hideSystemShadows() {
+    try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && this is Activity)
+            window.isNavigationBarContrastEnforced = false
+    } catch (e: NoSuchMethodError) {
+    }
+}
 
 fun ViewDataBinding.themeBinding(context: Context) {
 
     when (this) {
         is ActivityMainBinding -> {
-
+            context.hideSystemShadows()
         }
         is FragmentCharacterListBinding -> {
 
